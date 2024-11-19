182ee66bdf94b2b106109cb85d74e33637aacaf4
Junsu001 <junsu2752@gmail.com>
2024-11-19 19:46:50 +0900

    update: 부서관련 검색 구현중

--- src/main/java/com/gd/mystore/service/DepartmentService.java
+++ src/main/java/com/gd/mystore/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.gd.mystore.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.gd.mystore.dto.DepTransferDto;
 import com.gd.mystore.dto.DepartmentDto;
@@ -10,5 +11,8 @@ public interface DepartmentService {
 	List<DepartmentDto> selectMemberList();
 
 	List<DepTransferDto> selectChangeHistory(String startDate, String endDate, String searchType, String searchValue);
+
+	List<DepartmentDto> selectSearchEmployeeByName(String empName);
+
 
 }
