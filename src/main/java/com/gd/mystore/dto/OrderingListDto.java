487d9687b0e075bb522ee411a70cac756fc3eb2f
Sanghyeok-gwak <sb02380@naver.com>
2024-11-20 15:38:09 +0900

    config ordering관련 추가 , ordering 관련 Dto 작성

--- src/main/java/com/gd/mystore/dto/OrderingListDto.java
+++ src/main/java/com/gd/mystore/dto/OrderingListDto.java
@@ -1,17 +1,32 @@
 package com.gd.mystore.dto;
 
-import java.sql.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 
 public class OrderingListDto {
 	private int orderNo;
-	private String empName;
-//	ORDER_PRODUCT	VARCHAR2(500 BYTE)
+	private String empNo;
 	private String branchName;
 	private String orderStatus;
-	public String creater;
-	public Date createDate;
-	public String modifier;
-	public Date modifyDate;
-	public String deliveryDate;
-
+	private String orderCheckDate;
+	private String createDate;
+	private String modifier;
+	private String modifyDate;
+	private String useYn;
+	private List<OrderingProductDto> productList;
+	
 }
--- src/main/java/com/gd/mystore/dto/OrderingProductDto.java
+++ src/main/java/com/gd/mystore/dto/OrderingProductDto.java
@@ -1,13 +1,26 @@
 package com.gd.mystore.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+
 public class OrderingProductDto {
-//	PRODUCT_NO	NUMBER
-	private int orderingNo;
-//	ORDER_COUNT	NUMBER
-//	EMP_NO	NUMBER
-//	CREATE_DATE	DATE
-//	MODIFIER	NUMBER
-//	MODIFY_DATE	DATE
-//	USE_YN	CHAR(1 BYTE)
-//	EVENT	CHAR(1 BYTE)
+	private int productNo;
+	private int orderCount;
+	private String createDate;
+	private String modifier;
+	private String modifyDate;
+	private String useYn;
+	private int orderNo;
+	private String productName;
 }
