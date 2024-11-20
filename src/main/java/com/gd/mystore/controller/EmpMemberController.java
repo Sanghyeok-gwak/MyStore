df283ad0f277e0f977ddb1d28b453be88f3ad5d5
Chuntoo <yechan9981@naver.com>
2024-11-20 09:17:13 +0900

    update : SystemController 패치
    천예찬
    update : SystemController 패치
    import문 최적화

--- src/main/java/com/gd/mystore/controller/EmpMemberController.java
+++ src/main/java/com/gd/mystore/controller/EmpMemberController.java
@@ -2,19 +2,15 @@ package com.gd.mystore.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
