1a95d42ff9743749974ef43162bf41ae7decdac6
Chuntoo <yechan9981@naver.com>
2024-11-25 17:53:39 +0900

    update : 스케줄러 기능 제작중
    천예찬
    update : 스케줄러 기능 제작중

---
+++ src/main/java/com/gd/mystore/scheduler/WorkScheduler.java
@@ -0,0 +1,28 @@
+package com.gd.mystore.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WorkScheduler {
+	
+//	private final 
+	
+//	@Scheduled
+//	public void execute1() {
+//		log.debug(null);
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
