13fecf2385ab2e150a761c1f9e524804c9cbfbd0
Sanghyeok-gwak <sb02380@naver.com>
2024-11-26 18:00:39 +0900

    쪽지 관련 dao dto service ...... 추가

---
+++ src/main/java/com/gd/mystore/dao/NoteDao.java
@@ -0,0 +1,14 @@
+package com.gd.mystore.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Repository
+public class NoteDao {
+
+}
---
+++ src/main/java/com/gd/mystore/serviceimpl/NoteServiceImpl.java
@@ -0,0 +1,5 @@
+package com.gd.mystore.serviceimpl;
+
+public class NoteServiceImpl {
+
+}
